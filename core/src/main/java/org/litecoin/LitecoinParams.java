f74fe7ede42025c4e9fdece2115fe4fe17798136
Matt Corallo <git@bluematt.me>
2014-01-02 01:01:52 -0800

    Handle URI scheme

--- core/src/main/java/org/litecoin/LitecoinParams.java
+++ core/src/main/java/org/litecoin/LitecoinParams.java
@@ -107,6 +107,8 @@ public class LitecoinParams extends NetworkParameters {
         }
     }
 
+    @Override public String getURIScheme() { return "litecoin:"; }
+
     /** Gets the hash of the given block for the purpose of checking its PoW */
     public Sha256Hash calculateBlockPoWHash(Block b) {
         byte[] blockHeader = b.cloneAsHeader().bitcoinSerialize();
